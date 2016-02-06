e022859f57ac03b369fd20bb4aee2d35ba20ea68
prakyath <prakyathshankesi@gmail.com>
2016-02-06 18:17:40 +0530

    push

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '455870463');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'samudra.org.in');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
